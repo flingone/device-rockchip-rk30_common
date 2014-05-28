8ec113cb9b4cefc5f4de54c14e86388f9cab8bfe
Fling One <onefling8@gmail.com>
2014-05-28 15:41:43 +0800

    Initial import from sdk422
    Change-Id: I7aa295c42a313e2ada500ec9c90208c8829a8306

---
+++ common.mk
@@ -0,0 +1,27 @@
+#Rockchip Hardware SO
+hardware := $(LOCAL_PATH)/proprietary/hardware
+PRODUCT_COPY_FILES +=\
+	$(hardware)/hwcomposer.rk30board.so:obj/lib/hwcomposer.rk30board.so\
+	$(hardware)/hwcomposer.rk30board.so:system/lib/hwcomposer.rk30board.so\
+	$(hardware)/hwcomposer.rk30board.so:system/lib/hw/hwcomposer.rk30board.so\
+	$(hardware)/gralloc.rk30board.so:obj/lib/gralloc.rk30board.so\
+	$(hardware)/gralloc.rk30board.so:system/lib/gralloc.rk30board.so\
+	$(hardware)/gralloc.rk30board.so:system/lib/hw/gralloc.rk30board.so\
+	$(hardware)/libyuvtorgb.so:obj/lib/libyuvtorgb.so\
+	$(hardware)/libyuvtorgb.so:system/lib/libyuvtorgb.so\
+	$(hardware)/audio.primary.rk30board.so:obj/lib/audio.primary.rk30board.so\
+	$(hardware)/audio.primary.rk30board.so:system/lib/audio.primary.rk30board.so\
+	$(hardware)/audio.primary.rk30board.so:system/lib/hw/audio.primary.rk30board.so\
+	$(hardware)/audio_policy.rk30board.so:obj/lib/audio_policy.rk30board.so\
+	$(hardware)/audio_policy.rk30board.so:system/lib/audio_policy.rk30board.so\
+	$(hardware)/audio_policy.rk30board.so:system/lib/hw/audio_policy.rk30board.so\
+	$(hardware)/alsa.default.so:obj/lib/alsa.default.so\
+	$(hardware)/alsa.default.so:system/lib/alsa.default.so\
+	$(hardware)/alsa.default.so:system/lib/hw/alsa.default.so\
+	$(hardware)/acoustics.default.so:obj/lib/acoustics.default.so\
+	$(hardware)/acoustics.default.so:system/lib/acoustics.default.so\
+	$(hardware)/acoustics.default.so:system/lib/hw/acoustics.default.so\
+
+#Rockchip BIN
+bin := $(LOCAL_PATH)/proprietary/bin
+PRODUCT_COPY_FILES +=\
---
+++ proprietary/hardware/Android.mk
@@ -0,0 +1,5 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_PREBUILT_LIBS := hwcomposer.rk30board.so gralloc.rk30board.so libyuvtorgb.so audio.primary.rk30board.so audio_policy.rk30board.so alsa.default.so acoustics.default.so
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_MULTI_PREBUILT)
