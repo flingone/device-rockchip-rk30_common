657d2d216338ae5906bff5a6c22c12c0274b029e
AZ technology <aztec@localhost.domain>
2014-06-25 17:30:35 +0800

    Remove hwcomposer.rk30board.so
    This library will cause b2g coredump when first boot after flash.

--- common.mk
+++ common.mk
@@ -1,9 +1,6 @@
 #Rockchip Hardware SO
 hardware := $(LOCAL_PATH)/proprietary/hardware
 PRODUCT_COPY_FILES +=\
-	$(hardware)/hwcomposer.rk30board.so:obj/lib/hwcomposer.rk30board.so\
-	$(hardware)/hwcomposer.rk30board.so:system/lib/hwcomposer.rk30board.so\
-	$(hardware)/hwcomposer.rk30board.so:system/lib/hw/hwcomposer.rk30board.so\
 	$(hardware)/gralloc.rk30board.so:obj/lib/gralloc.rk30board.so\
 	$(hardware)/gralloc.rk30board.so:system/lib/gralloc.rk30board.so\
 	$(hardware)/gralloc.rk30board.so:system/lib/hw/gralloc.rk30board.so\
